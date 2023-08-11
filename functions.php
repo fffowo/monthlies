bebb5b544a623b438c2a9540ebc07b378b4d0644
Fo <123954300+fffowo@users.noreply.github.com>
2023-08-11 17:00:06 +0200

    Add files via upload

---
+++ functions.php
@@ -0,0 +1,50 @@
+
+<?php 
+function get_database_data() {
+    $data = array(
+        'servername' => "localhost",
+        'username' => "root",
+        'password' => "",
+        'dbname' => "monthlies"
+    );
+    return $data;
+}
+
+function connect(){
+    $servername = get_database_data()['servername'];
+    $username = get_database_data()['username'];
+    $password = get_database_data()['password'];
+    $dbname = get_database_data()['dbname'];
+
+    // Create connection
+    $conn = new mysqli($servername, $username, $password, $dbname);
+    // Check connection
+    if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
+    }
+    return $conn;
+}
+
+function submit_to_db($submitted_amount, $date)
+{
+    $sql = "INSERT INTO monthlies(date, amount) VALUES('" . $date . "', '" . $submitted_amount . "')";
+    connect()->query($sql);
+    connect()->close();
+}
+
+
+
+function get_totals($month) {
+    $sql = "SELECT SUM(amount) FROM monthlies WHERE date LIKE '" . $month . "%';";
+    $result = connect()->query($sql);
+
+    if ($result->num_rows > 0) {
+        foreach ($result as $amount) {
+            //var_dump($amount);
+            return $amount['SUM(amount)'];
+        }
+    }
+    connect()->close();
+}
+
+?>
